f4eba336cb9947ebb4063a031d3c52d139775dac
AntoineNEBOUT <Antoine.NEBOUT@protonmail.com>
2023-05-07 10:25:50 +0200

    New SplashScreen

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QSplashScreen>
+#include <QPixmap>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QPixmap pixmap("");
+    QSplashScreen splash(pixmap, Qt::FramelessWindowHint);
+    splash.show();
+
     MainWindow w;
     w.show();
     return a.exec();
